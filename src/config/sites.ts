39b313be75907883a7925511b3fac244ef0b5425
DevSaalih <soliualley@gmail.com>
2023-12-21 01:56:10 +0100

    Update

--- src/config/sites.ts
+++ src/config/sites.ts
@@ -16,7 +16,7 @@ const sites: Record<string, SiteConfig[]> = {
         viewId: 'panel'
       },
       iottwinmaker: {
-        sceneId: 'turbidity_scene',
+        sceneId: 'full_scene',
         workspaceId: 'aquaculture-twinmaker'
       },
       id: crypto.randomUUID(),
